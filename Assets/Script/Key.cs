05a3d303ced01707d012b99c46f46cda8c288750
Lib20 <67936804+prg217@users.noreply.github.com>
2022-07-14 16:47:32 +0900

    7_14_1

--- Assets/Script/Key.cs
+++ Assets/Script/Key.cs
@@ -13,16 +13,35 @@ public class Key : MonoBehaviour, IDropHandler
         var skill = eventData.pointerDrag.GetComponent<Skill>();
 
         //여기서 키 타입을 바꾸게 하고 스킬쪽에서 키 타입에 따라 그 버튼을 누르면 그 스킬쓰게 하면 안돼?
+        MyKeyCall(skill);
+        SkillCall(skill);
+
+    }
+    
+    void MyKeyCall(Skill skill)
+    {
         var parent = GameObject.Find(skill.keyType.ToString()); //열거형에 부모 이름을 저장해뒀으니 그걸로찾아줌
         mySkill.transform.SetParent(parent.transform);
         mySkill.transform.position = parent.transform.position;
         mySkill.key = parent.GetComponent<Key>(); //키 바꿔주기
-                                                  //Skill에서 열거형도 바꿔줘야함 : 부모 이름을 받아서 열거형 바꾸기?
+        mySkill.EnumChange();
+        //Debug.Log(mySkill);
+        //Debug.Log(mySkill.key);
+        Debug.Log(mySkill.name + "을 " + parent.name + "으로 넘겨줌");
+    }
 
+    void SkillCall(Skill skill)
+    {
         mySkill = skill; //기존꺼 처리 끝내고 이제 mySkill의 자리를 차지함
         skill.key = this;
         skill.transform.SetParent(this.transform);
         skill.transform.position = this.transform.position;
-        //Skill에서 열거형도 바꿔줘야함 : 부모 이름을 받아서 열거형 바꾸기?
+        skill.EnumChange();
+        //Debug.Log(skill);
+        //Debug.Log(skill.key);
+        Debug.Log(mySkill.name + "으로 " + gameObject.name + "에 등록");
+        //처음에는 직접 부모의 이름으로 하려고 했지만, (참고 자료 : https://magicmon.tistory.com/102)
+        //Enum.Parse가 왜인지 안돼 일단, Skill쪽에서 if문으로 관리하게 해줬다.
+        //나중에 어떤게 더 효율적인지 한 번 시도해볼 것.
     }
 }
--- Assets/Script/Skill.cs
+++ Assets/Script/Skill.cs
@@ -22,7 +22,7 @@ public class Skill : MonoBehaviour, IDragHandler, IEndDragHandler
     public KeyType keyType;
     public Key key;
 
-    void EnumChange()
+    public void EnumChange() //Key쪽에서 스킬을 움직일때마다 호출, 여기서 enum KeyType을 바꿔준다. 
     {
         if (key.name == "Q")
         {
@@ -31,6 +31,38 @@ public class Skill : MonoBehaviour, IDragHandler, IEndDragHandler
         else if (key.name == "W")
         {
             keyType = KeyType.W;
+        }
+        else if (key.name == "E")
+        {
+            keyType = KeyType.E;
+        }
+        else if (key.name == "R")
+        {
+            keyType = KeyType.R;
+        }
+        else if (key.name == "T")
+        {
+            keyType = KeyType.T;
+        }
+        else if (key.name == "A")
+        {
+            keyType = KeyType.A;
+        }
+        else if (key.name == "S")
+        {
+            keyType = KeyType.S;
+        }
+        else if (key.name == "D")
+        {
+            keyType = KeyType.D;
+        }
+        else if (key.name == "C")
+        {
+            keyType = KeyType.C;
+        }
+        else if (key.name == "F")
+        {
+            keyType = KeyType.F;
         }
     }
 
@@ -43,6 +75,5 @@ public class Skill : MonoBehaviour, IDragHandler, IEndDragHandler
     public void OnEndDrag(PointerEventData eventData)
     {
         GetComponent<Image>().raycastTarget = true;
-        EnumChange();
     }
 }
